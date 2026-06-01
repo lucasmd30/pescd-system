a48ebdba4ac2512c693b10d59746e446d70b54aa
Lucas Martinez <lucas.domingues@catijr.com>
2026-06-01 09:45:29 -0300

    Initial project setup with Spring Boot

---
+++ src/main/java/br/ufscar/pescd/PescdApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.pescd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PescdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PescdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufscar/pescd/PescdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.pescd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PescdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
